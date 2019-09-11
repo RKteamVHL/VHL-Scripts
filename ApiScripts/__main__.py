2659263133537bb13162f1ffaadde1348413c124
seanD111 <sean_delong@hotmail.ca>
2019-09-11 17:58:22 -0400

    added caching of varint results into a graph; added evidences + related phenotypes

--- ApiScripts/__main__.py
+++ ApiScripts/__main__.py
@@ -1,4 +1,7 @@
 from civicpy import civic
+import hgvs.parser
+import networkx as nx
+import obonet
 import argparse
 import time
 import json
@@ -7,31 +10,88 @@ import os
 
 import pprint
 
-# 58 is gene VHL
+# url for human-phenotype-ontology
+HPO_OBO_URL = "http://purl.obolibrary.org/obo/hp.obo"
+
+GRAPH_FILENAME = "civic_data.gml"
+
+# keys to save in the networkx structure
+VARIANT_KEYS = [
+	"evidenceAccepted",
+	"evidenceSubmitted",
+	"associatedPhenotypes"
+
+]
+
+# 58 corresponds to the VHL gene 
 GENE_ID =  58
+
+# should include submitted, along with accepted, evidence statements 
+INCLUDE_SUBMITTED = False
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 
 
-	# parser.add_argument('-exp', '--experiment_directory', help = '''Experiment Directory''')
-	# parser.add_argument('-s', '--save', help = '''Save all databases localy.''', action="store_true")
+	parser.add_argument('-d', '--directory', help = '''Local driectory to save graph''', default="")
+	parser.add_argument('-u', '--update', help = '''Update the local variants from civic''', action="store_true")
 
-	gene = civic.get_gene_by_id(GENE_ID)
-	#making a dictionary of variant types
-	variant_type_dict = {}
+	args = parser.parse_args()
 
-	for variant in gene.variants:
-		for var_type in variant.types:
-			if not var_type.name in variant_type_dict:
-				variant_type_dict[var_type.name] = []
-			variant_type_dict[var_type.name].append(variant.name)
-			# using the actual civic ids instead of mutation type names
-			# if not var_type.id in variant_type_dict:
-			# 	variant_type_dict[var_type.id] = []
-			# variant_type_dict[var_type.id].append(variant)
+	variant_graph = nx.Graph()
+	if args.update:
+		gene = civic.get_gene_by_id(GENE_ID)
 
-	pprint.pprint(variant_type_dict)
+		#information relating to HPO terms
+		# hpo = obonet.read_obo(HPO_OBO_URL)
+		# assert nx.is_directed_acyclic_graph(hpo)
+		# print('Number of HPO terms: {}'.format(len(hpo)))
+
+
+		print('Number of variants for VHL: {}'.format(len(gene.variants)))
+		for variant in gene.variants:
+			variant_graph.add_node(variant.id)
+			variant_node = variant_graph.nodes[variant.id]
+
+			#finding how many evidence items exist for the variant.
+			#TODO: actually verify that evidence: supports, is germline, and is case study
+			variant_node["evidenceAccepted"] = ["NA"]
+			variant_node["evidenceSubmitted"] = ["NA"]
+			variant_node["associatedPhenotypes"] = ["NA"]
+			for evidence in variant.evidence_items:
+				phenotypes = [phenotype.hpo_id for phenotype in evidence.phenotypes]
+				if evidence.status == "submitted":
+					variant_node["evidenceSubmitted"].append(evidence.id)
+					if INCLUDE_SUBMITTED:
+						variant_node["associatedPhenotypes"].extend(phenotypes)
+				elif evidence.status == "accepted":
+					variant_node["evidenceAccepted"].append(evidence.id)	
+					variant_node["associatedPhenotypes"].extend(phenotypes)
+
+						
+			
+
+
+
+		nx.write_gml(variant_graph, os.path.join(args.directory, GRAPH_FILENAME))
+
+	else:
+
+		variant_graph = nx.read_gml(os.path.join(args.directory, GRAPH_FILENAME))
+
+
+		#making a dictionary of variant types
+		variant_type_dict = {}
+			# for var_type in variant.types:
+			# 	if not var_type.name in variant_type_dict:
+			# 		variant_type_dict[var_type.name] = []
+			# 	variant_type_dict[var_type.name].append(variant.name)
+				# using the actual civic ids instead of mutation type names
+				# if not var_type.id in variant_type_dict:
+				# 	variant_type_dict[var_type.id] = []
+				# variant_type_dict[var_type.id].append(variant)
+
+		pprint.pprint(variant_graph)
 
 # QUESTIONS:
 # - where do you find the keys each attribute type has?
