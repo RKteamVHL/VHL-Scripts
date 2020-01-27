fb03f70a85f8e64a9e37789f03216ff4e7f9822f
seanD111 <sean_delong@hotmail.ca>
2020-01-27 15:12:14 -0500

    added phenotype summary code

---
+++ Analysis/__main__.py
@@ -0,0 +1,119 @@
+from .clustering.VariantGraph import VariantGraph
+from .plotting.GraphPlotter import GraphPlotter
+import matplotlib.pyplot as plt
+import argparse
+from snf import compute
+
+COLORMAP= {
+	0:(0,0,0,0.5), 
+	1:(0,0,1,0.5),
+	2:(0,1,0,0.5),
+	3:(0,1,1,0.5),
+	4:(0,1,0,0.5),
+	5:(0,1,1,0.5),
+	6:(1,0,0,0.5),
+	7:(1,0,1,0.5),
+
+	
+}
+
+FILE_OUTPUT = "output/"
+
+PRE_CACHE = "cache/variant_nodes.json"
+POST_CACHE = "cache/variant_nodes_with_similarities.json"
+
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument('similarity_type', help = '''The type of similarity to base clustering on''')
+	parser.add_argument('-d', '--directory', help = '''Local directory to save graph''', default="")
+	parser.add_argument('-f', '--filter', help='''Removes nodes that don't have values for the filter key''')
+	parser.add_argument('-c', '--cluster', help='''Forces spectral clustering to use this many clusters''')
+	parser.add_argument('-rc', '--pre_cache', help = '''Use the local graph raw node cache''', action="store_true")
+	parser.add_argument('-oc', '--post_cache', help = '''Use the local graph calulated node cache''', action="store_true")
+	parser.add_argument('-is', '--ignore_submitted', help = '''If set, ignores unreviewed variants (civic)''', action="store_true")
+
+	args = parser.parse_args()
+
+	VG = VariantGraph()
+
+	#fetch/process all relevant data from all sources
+	if not args.pre_cache:
+
+		VG.add_nodes_from_db('Civic')
+		VG.add_nodes_from_db('KimStudents2019')
+		VG.add_nodes_from_db('Gnomad')
+		VG.add_nodes_from_db('ClinVar')
+		VG.merge_nodes()
+		VG.save_to_json_file(PRE_CACHE)
+
+	else:
+		VG.load_from_json_file(PRE_CACHE)
+
+
+	# NOTE: these lines for making new graph PG are temporary,
+	# and should only be used for filtering the nodes
+	PG = VariantGraph()
+
+	# PG.add_nodes_from([(n, d) for n, d in VG.nodes(data=True) if d['all']['associatedPhenotypes']]) # needs pheno
+	# PG.add_nodes_from([(n, d) for n, d in VG.nodes(data=True) if d['all']['variantTypes']]) #needs var type
+	if args.filter:
+		PG.add_nodes_from([(n, d) for n, d in VG.nodes(data=True) if d['all'][args.filter]]) # needs filter
+	else:
+		PG = VG # use everything
+
+	PG.add_edges_from([(n1, n2, d) for n1, n2, d in VG.edges(data=True) if n1 in PG and n2 in PG])
+
+
+	# PG.save_to_json_file("filtered_variant_nodes.json")
+	PG.save_to_text_file("variant_names.tsv")
+	if not args.post_cache:
+		PG.calculate_node_attributes()
+		PG.calculate_similarities()
+		PG.save_to_json_file(POST_CACHE)
+	else:
+		PG.load_from_json_file(POST_CACHE)
+
+	adj_mat = PG.get_adjacency_mats(types=[args.similarity_type])
+
+	clust_count1, clust_count2 = compute.get_n_clusters(adj_mat[0])
+
+	if args.cluster:
+		clust_count1 = int(args.cluster)
+		print(f"Forcing {clust_count1} clusters")
+	else:
+		print(f"Cluster estimates: {clust_count1}, {clust_count2}")
+
+
+
+	PG.cluster_by(args.similarity_type, num_clusters=clust_count1)
+	PG.aggregate_cluster(args.similarity_type)
+	PG.save_to_json_file(f"{FILE_OUTPUT}{args.similarity_type}_variant_nodes.json", nodes_only=True)
+	# TODO: put this plotting in the GraphPlotter class
+	# Get newly sorted adjmat
+	sorted_adj_mat = PG.get_adjacency_mats(types=[args.similarity_type])
+	plt.matshow(sorted_adj_mat[0])
+	plt.savefig(f'{FILE_OUTPUT}{args.similarity_type}_heatmap')
+	plt.show()
+
+
+
+	plotter = GraphPlotter(graph=PG)
+	plotter.count_nodes_by(args.filter)
+
+	# plotting the network isn't too informative
+	# nx.draw(PG, 
+	# 	node_size=100, 
+	# 	with_labels=True,		
+	# 	pos=nx.spring_layout(PG),
+	# 	width=0.001,
+	# 	node_color=[COLORMAP[d[f'{args.similarity_type}_label']] for (u,d) in PG.nodes(data=True)]
+	# )
+	# plt.show()
+
+
+# QUESTIONS:
+# - where do you find the keys each attribute type has?
+# - is there a list of ids for mutation types (Civic Attribute)
--- Analysis/plotting/GraphPlotter.py
+++ Analysis/plotting/GraphPlotter.py
@@ -1,16 +1,73 @@
 import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 
 class GraphPlotter:
-	def __init__(self):
+	def __init__(self, *args, **kwargs):
 		self.graph = None
+		if "graph" in kwargs:
+			self.set_graph(kwargs["graph"])
 
 	def set_graph(self, graph):
 		assert(nx.Graph in type(graph).mro())
 		self.graph = graph
 
-	def summarize_nodes_by(self, field):
-		pass
-	# TODO: Implement all previous visualizations
+	def count_nodes_by(self, field):
+
+		data_sums = {}
+
+		for node, n_data in self.graph.nodes(data=True):
+			all_node_data =  n_data['all']
+			if field in all_node_data:
+				if isinstance(all_node_data[field], list):
+					for v in all_node_data[field]:
+						data_sums[v] = data_sums.get(v, 0) + 1
+				elif isinstance(all_node_data[field], str):
+					data_sums[all_node_data[field]] = data_sums.get(all_node_data[field], 0) + 1
+
+		_values = []
+		_labels = []
+		for k, v in data_sums.items():
+			# filter values
+			if v > 3:
+				_values.append(v)
+				_labels.append(k)
+
+
+		i_sorted = np.argsort(_values, axis=0)
+
+		sorted_values = np.take_along_axis(np.array(_values), i_sorted, 0)
+		sorted_labels = np.take_along_axis(np.array(_labels), i_sorted, 0)
+
+
+		# set width of bar
+		barWidth = 1
+
+
+
+		# Set position of bar on X axis
+		r1 = np.arange(len(sorted_values))
+		# r2 = [x + barWidth for x in r1]
+		# r3 = [x + barWidth for x in r2]
+
+		# Make the plot
+		plt.bar(r1, sorted_values, color='#7f6d5f', width=barWidth, edgecolor='white', label='var1')
+		# plt.bar(r2, bars2, color='#557f2d', width=barWidth, edgecolor='white', label='var2')
+		# plt.bar(r3, bars3, color='#2d7f5e', width=barWidth, edgecolor='white', label='var3')
+
+		# Add xticks on the middle of the group bars
+		plt.xlabel(field, fontweight='bold')
+		plt.xticks(r1, sorted_labels, rotation='vertical')
+
+		plt.ylabel("# of variants", fontweight='bold')
+
+		# Create legend & Show graphic
+		plt.legend()
+		plt.show()
+
+# TODO: Implement all previous visualizations
 	# 1) phenotype vs # variants
 	# 2) phenotype vs phenotype correlation matrix
 	# 3) phenotype + mutation type vs # of variants
