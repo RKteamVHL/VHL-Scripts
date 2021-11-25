f901fe7ff34b075acfeaf7a1878dae2dc5cc5f63
sean <sean_delong@hotmail.ca>
2021-11-25 18:20:37 -0500

    removed non-essential decision tree code

--- Analysis/features/kimstudents_dataframe_cnn.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from . import constants
-
-
-def dataframe_decisiontree(df, analysis_type):
-    fig_path = os.path.join(constants.FIGURE_DIR, analysis_type)
-    df = df.dropna(subset=['codon_start']).fillna(0)
